59ec1d968bde5a0ea35edd3cb58960079589f3ac
pvelu1 <147877198+pvelu1@users.noreply.github.com>
2023-10-26 15:56:12 -0700

    Add files via upload

---
+++ CT1.java
@@ -0,0 +1,7 @@
+public class CT1 {
+    public static void main (String [] args) {
+      System.out.println("Olive Garden Italian Restaurant");
+      System.out.println("1617 W Bethany Home Rd");       
+      System.out.println("Phoenix, AZ 85015");
+   }
+}
